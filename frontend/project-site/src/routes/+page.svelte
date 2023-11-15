621a61dcb5fe244eb92530a56eb37eb7dae1b051
martin <koo.martin@gmail.com>
2023-11-15 09:37:00 -0500

    add down arrow icon with too fancy animation

--- frontend/project-site/src/routes/+page.svelte
+++ frontend/project-site/src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
 	import WorksGrid from './works/Grid.svelte';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import ArrowDownIcon from '~icons/mdi/KeyboardArrowDown';
+
+	let DownIconElement;
+
+
 </script>
 
 <svelte:head>
@@ -12,6 +14,7 @@
 
 <section class="hero">
 		<h1>This is a cool graphic</h1>
+		<div class="arrow-down-scroll-indicator" bind:this={DownIconElement}><ArrowDownIcon style="font-size: 3em" /></div>
 </section>
 
 <section class="main">
@@ -49,19 +52,72 @@
 		background: white;
 	}
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
+	.arrow-down-scroll-indicator {
+		position: fixed;
+		width: 100vw;
+		display: flex;
+		justify-content: center;
+		bottom: 40px;
+
+		-webkit-animation: flip-in 0.3s ease-out both, heartbeat 0.3s ease-in-out 2 both;
+		animation: flip-in 0.3s ease-out both, heartbeat 0.3s ease-in-out 2 both;
+		animation-delay: 1s, 1.5s;
 	}
 
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+	/* Icon animations */
+	/* flip in */
+	@-webkit-keyframes flip-in {
+		0% {
+			-webkit-transform: rotateY(90deg);
+					transform: rotateY(90deg);
+			opacity: 0;
+		}
+		100% {
+			-webkit-transform: rotateY(0);
+					transform: rotateY(0);
+			opacity: 1;
+		}
+	}
+	@keyframes flip-in {
+		0% {
+			-webkit-transform: rotateY(90deg);
+					transform: rotateY(90deg);
+			opacity: 0;
+		}
+		100% {
+			-webkit-transform: rotateY(0);
+					transform: rotateY(0);
+			opacity: 1;
+		}
+	}
+	/* heartbeat */
+	@-webkit-keyframes heartbeat {
+		/* 0% {
+			-webkit-transform: scale(1);
+					transform: scale(1);
+		} OVERWRITES 0% FROM PREVIOUS ANIMATION*/
+		
+		50% {
+			-webkit-transform: scale(1.3);
+					transform: scale(1.3);
+		}
+		100% {
+			-webkit-transform: scale(1);
+					transform: scale(1);
+		}
+	}
+	@keyframes heartbeat {
+		/* 0% {
+			-webkit-transform: scale(1);
+					transform: scale(1);
+		} OVERWRITES 0% FROM PREVIOUS ANIMATION */
+		50% {
+			-webkit-transform: scale(1.3);
+					transform: scale(1.3);
+		}
+		100% {
+			-webkit-transform: scale(1);
+					transform: scale(1);
+		}
 	}
 </style>
