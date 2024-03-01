55f3e637aac13b0a8484f293026eda9fb88f9d34
mkoo21 <koo.martin@gmail.com>
2024-03-01 10:05:44 -0500

    pixi start

---
+++ frontend/pixi-gallery/src/lib/components/Header.svelte
@@ -0,0 +1,31 @@
+<script>
+    import DotsHorizontal from "~icons/mdi/dots-horizontal";
+    
+</script>
+
+<header>
+    <div class="left cell">
+        <span class="hbtn" style=""><DotsHorizontal style="font-size:1.5em"  /></span>
+    </div>
+    <div class="middle"></div>
+    <div class="right"></div>
+</header>
+
+<style lang="scss">
+    header {
+        display: flex;
+        justify-content: space-between;
+        background-color: rgba(0,0,0,0.7);
+        height: var(--header-height);
+    }
+    .cell {
+        height: 100%;
+        display: flex;
+        align-items:center;
+        padding: 0 10px 0 10px;
+        color: white;
+    }
+    .left {
+        padding-left: 14px;
+    }
+</style>
---
+++ frontend/pixi-gallery/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>Pixi Library</title>
+	<meta name="description" content="Library for fun 2d canvas widgets" />
+</svelte:head>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
